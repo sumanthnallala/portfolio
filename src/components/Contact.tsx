3fb2b99a7743474994e2939958788fa208feb8e1
Sumanth Nallala <nallala.sumanthreddy111@gmail.com>
2025-03-06 13:00:52 +0530

    installed axios

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,12 +3,13 @@ import "./Contact.scss";
 const Contact = () => {
   return (
     <div className="contact-content-container">
-      <h1>Contact</h1>
-      <h2>Feel free to reach out to me!</h2>
-      <input type="text" placeholder="Name" />
-      <input type="email" placeholder="Email" />
-      <textarea placeholder="Message" />
-      <button>Send</button>
+      <h2>Contact</h2>
+      <p>Feel free to reach out to me!</p>
+      <input className="input-container" type="text" placeholder="Name" />
+      <input className="input-container" type="email" placeholder="Email" />
+      <input className="input-container" type="subject" placeholder="Subject" />
+      <textarea className="textarea-container" placeholder="Message" />
+      <button className="send-button-container">Send</button>
     </div>
   );
 };
